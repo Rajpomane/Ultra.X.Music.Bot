e0dce5127592639b1bcde0a55679615acdaa0d09
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-07-25 14:34:57 +0530

    Add files via upload

--- Deepak/main.py
+++ Deepak/main.py
@@ -4,7 +4,7 @@ import random
 import asyncio
 import telethon.utils
 from telethon import TelegramClient, events
-from config import API_HASH, API_ID, BOT_TOKEN, SESSION_NAME
+from config import API_HASH, API_ID, BOT_TOKEN, DEEPAK_SESSION
 from pyrogram import Client
 from pytgcalls import PyTgCalls
 
@@ -19,12 +19,12 @@ bot = Client(
 BOT = TelegramClient('BOT', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
 user = Client(
-    SESSION_NAME,
+    DEEPAK_SESSION,
     api_id=API_ID,
     api_hash=API_HASH,
 )
 
-Test = Client(SESSION_NAME, api_id=API_ID, api_hash=API_HASH, plugins={'root': 'Deepak.Player'})
+Test = Client(DEEPAK_SESSION, api_id=API_ID, api_hash=API_HASH, plugins={'root': 'Deepak.Player'})
 call_py = PyTgCalls(
     Test,
     cache_duration=100,
